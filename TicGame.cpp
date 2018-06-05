de7c976165dbbd0f2bbdc457e779677478844da1
Vivek Raman <vr.ac4bf@live.com>
2018-06-05 16:17:38 +0530

    Created Game class and set up variables

--- TicGame.cpp
+++ TicGame.cpp
@@ -1,9 +1,36 @@
 #include "TicGame.h"
+#include "stdafx.h"
 
-
+typedef int Coords[2];
+constexpr int BOARD_SIZE = 3;
 
 TicGame::TicGame()
 {
+	// Set board to NULL
+	for (int i = 0; i < BOARD_SIZE; ++i)	
+	{
+		for (int j = 0; j < BOARD_SIZE; ++j)
+		{
+			Game[i][j] = '\0';
+		}
+	}
+	InputCoords[0] = 0;
+	InputCoords[1] = 0;
+	ScoreX = ScoreY = 0;
+}
+
+int TicGame::GetScoreX() const { return ScoreX; }
+int TicGame::GetScoreY() const { return ScoreY; }
+
+int TicGame::PlayTurn(int Input, bool bIsTurnX)
+{
+	
+	return 0;
+}
+
+bool TicGame::IsGameDone()
+{
+	return false;
 }
 
 
--- TicGame.h
+++ TicGame.h
@@ -1,7 +1,25 @@
 
+typedef int Coords[2];
 class TicGame
 {
+
 public:
-	TicGame();
+	TicGame();	//Constructor to set default values
+
+	//Get functions
+	int GetScoreX() const;
+	int GetScoreY() const;
+
+	int PlayTurn(int, bool);	//TODO: Improve return value
+	bool IsGameDone();
+
+
+
 	~TicGame();
+
+private:
+	char Game[3][3];
+	Coords InputCoords;
+	int ScoreX;
+	int ScoreY;
 };
